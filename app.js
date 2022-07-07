2d955d968499713e3154fa37fc5d1a1e5f392e92
ZorroFeroz <juanstrack@gmail.com>
2022-07-07 20:56:53 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aquí Puchi, experimentando.'));
+app.get('/', (req, res) => res.send('Esta vez, Puchi siente que sabe lo que hace.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
